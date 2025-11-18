69a15d80a524464570b20598b3984d9129029dfc
DoggiKim <kmss0426@hanyang.ac.kr>
2025-11-18 15:25:41 +0900

    chore: add responsive design system

--- src/components/Home/List/List.styles.ts
+++ src/components/Home/List/List.styles.ts
@@ -12,19 +12,39 @@ export const ListItem = styled.li`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  gap: 1.25rem;
+  gap: ${({ theme }) => theme.gaps.XXS.wide};
   white-space: pre-line;
   line-height: 1.75rem;
 `;
 
 export const ItemContent = styled.div`
   display: flex;
-  width: 33rem;
+  width: ${theme.componentWidths.large.wide};
   ${theme.fonts.body.l500}
   color: ${theme.colors.grayScale.black};
+  white-space: pre-line;
+  word-wrap: break-word;
+
+  ${theme.media.desktop} {
+    width: ${theme.componentWidths.large.desktop};
+  }
+
+  ${theme.media.tablet} {
+    width: ${theme.componentWidths.large.tablet};
+  }
+
+  ${theme.media.mobile} {
+    width: ${theme.componentWidths.large.mobile};
+    white-space: normal;
+    word-break: break-word;
+    line-height: 1.5;
+  }
 `;
 
 export const ItemButton = styled.div`
+  width: ${({ theme }) => theme.componentWidths.min.tablet};
   display: flex;
   align-items: center;
+  margin-left: ${({ theme }) => theme.gaps.XXS.tablet};
+  flex-shrink: 0;
 `;
