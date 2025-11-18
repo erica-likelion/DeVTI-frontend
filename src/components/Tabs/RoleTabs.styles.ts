e9805d529112f5ff7d7e1d22e37b375664789144
DoggiKim <kmss0426@hanyang.ac.kr>
2025-11-18 17:16:40 +0900

    chore: add responsive design system

--- src/components/Tabs/RoleTabs.styles.ts
+++ src/components/Tabs/RoleTabs.styles.ts
@@ -3,20 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: relative;
   display: flex;
-  gap: 1.25rem;
+  ${({ theme }) => theme.responsive.property.gap('M')}
   align-items: center;
   padding-bottom: 0.5rem; /* indicator 공간 확보 */
 `;
 
 export const TabButton = styled.button<{ isActive: boolean }>`
   position: relative;
-  padding-bottom: 0.25rem;
-  padding-top: 0.5rem;
-  padding-left: 1.25rem;
-  padding-right: 1.25rem;
+  ${({ theme }) => theme.responsive.property.paddingComplex('XXS', 'M', 'XXS', 'M')}
   background: none;
   border: none;
-
   color: ${({ theme }) => theme.colors.grayScale.black};
   ${({ theme }) => theme.fonts.body.m500};
 
@@ -31,6 +27,5 @@ export const Indicator = styled.div`
   bottom: 0;
   height: 0.25rem;
   background: ${({ theme }) => theme.colors.primary.VT500};
-
   transition: 0.25s ease;
 `;
