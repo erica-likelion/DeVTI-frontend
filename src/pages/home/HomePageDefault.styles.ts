7e30de8dbbc3f527f581e414efaf1060df0c59b3
DoggiKim <kmss0426@hanyang.ac.kr>
2025-11-19 19:45:01 +0900

    feat: add newroom and newroom code page layout

--- src/pages/home/HomePageDefault.styles.ts
+++ src/pages/home/HomePageDefault.styles.ts
@@ -1,34 +1,78 @@
 import styled from 'styled-components';
-import { theme } from '../../styles/theme';
 
 export const Container = styled.div`
-  gap: 5rem;
-  
-  ${theme.layouts.mobileCommon}
-  
-  ${theme.media.tablet} {
-    ${theme.layouts.tabletCommon}
-  }
-  
-  ${theme.media.desktop} {
-    ${theme.layouts.desktopCommon}
-  }
-  
-  ${theme.media.wide} {
-    ${theme.layouts.wideCommon}
-  }
+  ${({ theme }) => theme.responsive.property.gap('XXL')}
+  ${({ theme }) => theme.responsive.property.width('max')}
+  height: 100%;
+  overflow: hidden;
 `;
 
 export const Title = styled.h2`
-  ${theme.fonts.heading.h2}
-  color: ${theme.colors.grayScale.black};
+  ${({ theme }) => theme.fonts.heading.h2}
+  color: ${({ theme }) => theme.colors.grayScale.black};
   text-align: center;
 `;
 
 export const ListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  width: 100%;
+  height: calc(100vh - 8rem);
+  overflow-y: auto;
+  ${({ theme }) => theme.responsive.property.gap('L')}
+  background-color: ${({ theme }) => theme.colors.grayScale.black};
+  
+  ${({ theme }) => theme.responsive.property.padding('XXL')}
+  ${({ theme }) => theme.responsive.property.borderRadius('smooth')}
+`;
+
+export const ButtonSection = styled.div`
   display: flex;
-  width: 42.5rem;
-  padding: 3.75rem 0;
   flex-direction: column;
-  align-items: center;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.grayScale.white};
+  
+  ${({ theme }) => theme.responsive.property.gap('L')}
+  ${({ theme }) => theme.responsive.property.padding('L')}
+  ${({ theme }) => theme.responsive.property.borderRadius('smooth')}
+
+  h2 {
+    ${({ theme }) => theme.fonts.heading.h2}
+    color: ${({ theme }) => theme.colors.grayScale.black};
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+export const ButtonGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  ${({ theme }) => theme.responsive.property.gap('M')}
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export const InputTestSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.grayScale.white};
+  
+  ${({ theme }) => theme.responsive.property.gap('L')}
+  ${({ theme }) => theme.responsive.property.padding('L')}
+  ${({ theme }) => theme.responsive.property.borderRadius('smooth')}
+
+  h2 {
+    ${({ theme }) => theme.fonts.heading.h2}
+    color: ${({ theme }) => theme.colors.grayScale.black};
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+export const InputGrid = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${({ theme }) => theme.responsive.property.gap('M')}
+  width: 100%;
 `;
---
+++ src/pages/newroom/NewRoom.styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  ${({ theme }) => theme.responsive.property.width('large')}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  gap: 5rem;
+`;
+
+export const Title = styled.h2`
+  ${({ theme }) => theme.fonts.heading.h2}
+  color: ${({ theme }) => theme.colors.grayScale.black};
+`;
+
+export const ListWrapper = styled.div`
+  ${({ theme }) => theme.responsive.property.width('large')}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3.75rem;
+`;
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => theme.responsive.property.width('large')}
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+  margin: 0;
+`;
+
+export const Label = styled.div`
+  ${({ theme }) => theme.fonts.heading.h2}
+  ${({ theme }) => theme.responsive.property.width('min')};
+  margin: 0;
+  padding:0;
+`;
+
+export const Input = styled.div`
+  width: 100%;
+`;
+
+
+
+
