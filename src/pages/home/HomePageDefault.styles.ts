163fb498689761048c9278dad1ac164eab818ae9
DoggiKim <kmss0426@hanyang.ac.kr>
2025-11-19 19:48:41 +0900

    fix: remove test sections

--- src/pages/home/HomePageDefault.styles.ts
+++ src/pages/home/HomePageDefault.styles.ts
@@ -14,65 +14,10 @@ export const Title = styled.h2`
 `;
 
 export const ListWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
   width: 100%;
-  height: calc(100vh - 8rem);
-  overflow-y: auto;
-  ${({ theme }) => theme.responsive.property.gap('L')}
-  background-color: ${({ theme }) => theme.colors.grayScale.black};
-  
-  ${({ theme }) => theme.responsive.property.padding('XXL')}
-  ${({ theme }) => theme.responsive.property.borderRadius('smooth')}
-`;
-
-export const ButtonSection = styled.div`
   display: flex;
+  padding: 3.75rem 0;
   flex-direction: column;
-  width: 100%;
-  background-color: ${({ theme }) => theme.colors.grayScale.white};
-  
-  ${({ theme }) => theme.responsive.property.gap('L')}
-  ${({ theme }) => theme.responsive.property.padding('L')}
-  ${({ theme }) => theme.responsive.property.borderRadius('smooth')}
-
-  h2 {
-    ${({ theme }) => theme.fonts.heading.h2}
-    color: ${({ theme }) => theme.colors.grayScale.black};
-    margin: 0;
-    text-align: center;
-  }
+  align-items: center;
 `;
 
-export const ButtonGrid = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  ${({ theme }) => theme.responsive.property.gap('M')}
-  width: 100%;
-  align-items: flex-start;
-`;
-
-export const InputTestSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  background-color: ${({ theme }) => theme.colors.grayScale.white};
-  
-  ${({ theme }) => theme.responsive.property.gap('L')}
-  ${({ theme }) => theme.responsive.property.padding('L')}
-  ${({ theme }) => theme.responsive.property.borderRadius('smooth')}
-
-  h2 {
-    ${({ theme }) => theme.fonts.heading.h2}
-    color: ${({ theme }) => theme.colors.grayScale.black};
-    margin: 0;
-    text-align: center;
-  }
-`;
-
-export const InputGrid = styled.div`
-  display: flex;
-  flex-direction: column;
-  ${({ theme }) => theme.responsive.property.gap('M')}
-  width: 100%;
-`;
