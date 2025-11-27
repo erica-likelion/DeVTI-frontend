14731e01e38459a5546c2b4fc018131548c01582
kkeunii <greenlee55@naver.com>
2025-11-27 14:40:02 +0900

    fix: 테스트용 코드 수정

--- src/pages/home/HomePageDefault.tsx
+++ src/pages/home/HomePageDefault.tsx
@@ -7,7 +7,6 @@ export default function HomePage() {
     <S.Container>
       <S.Title>참여 중인 매칭룸</S.Title>
       <S.ListWrapper>
-        <WtLMemberList icon={img} header="김사자" keywords={[['낮', '대면'], ['PM'], ['배우면서 즐겁게']]} rightButton="꼬리 흔들기" disabled={false} onClick={() => console.log('Clicked!')} />
       </S.ListWrapper>
     </S.Container>
   );
