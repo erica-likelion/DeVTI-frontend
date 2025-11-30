8b64605c85aafbeb87f370772c3b35bafef1b9d5
DoggiKim <kmss0426@hanyang.ac.kr>
2025-11-30 20:59:36 +0900

    feat: add managejoin page

---
+++ src/pages/manageroom/ManageJoinPage.styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+${({ theme }) => theme.responsive.property.generalViewport()}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  gap: 5rem;
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => theme.fonts.heading.h1}
+  color: ${({ theme }) => theme.colors.grayScale.black};
+  white-space: pre-line;
+  text-align: center;
+  
+`;
+
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => theme.responsive.property.width('large')}
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+  margin: 0;
+`;
+
+export const Label = styled.div`
+  ${({ theme }) => theme.fonts.heading.h4};
+  ${({ theme }) => theme.responsive.property.width('min')};
+`;
+
+export const Input = styled.div`
+  width: 100%;
+`;
+
+
+
+
---
+++ src/pages/manageroom/ManageJoinPage.tsx
@@ -0,0 +1,33 @@
+import * as S from './ManageJoinPage.styles';
+import { InputField } from '@/components/Input';
+import BlackLTextButton from '@/components/ButtonStatic/BkLTextButton';
+import { useState, type ChangeEvent } from 'react';
+
+
+export default function ManageJoinPage() {
+    const [adminCode, setAdminCode] = useState('');
+    const correctAdminCode = 'admin123'; // 임시 정답 코드
+    const isValidCode = adminCode === correctAdminCode;
+
+    const handleAdminCodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setAdminCode(e.target.value);
+    };
+
+    return (
+        <S.Container>
+        <S.Title>매칭룸 관리하기</S.Title>
+            <S.InputWrapper>
+                <S.Label>운영진 입장 코드</S.Label>
+                <S.Input>
+                <InputField 
+                    variant='input'
+                    value={adminCode}
+                    onChange={handleAdminCodeChange}
+                    hasIcon={false}
+                />
+                </S.Input>
+            </S.InputWrapper>
+            <BlackLTextButton disabled={!isValidCode}>다음</BlackLTextButton>
+        </S.Container>
+    );
+}
