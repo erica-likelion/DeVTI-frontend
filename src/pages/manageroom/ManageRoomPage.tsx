39321c310a2251136662db8cfd2441990f343641
kkeunii <greenlee55@naver.com>
2025-12-20 15:36:23 +0900

    fix

--- src/pages/manageroom/ManageRoomPage.tsx
+++ src/pages/manageroom/ManageRoomPage.tsx
@@ -3,25 +3,33 @@ import ManageRoomDefault from './ManageRoomDefault';
 import ManageRoomMatched from './ManageRoomMatched';
 import { useRoomSocket } from '@/hooks/useRoomSocket';
 import { getCurrentRoomId } from '@/utils/globalState';
-
-const TEST_TOKEN = import.meta.env.VITE_TEST_AUTH_TOKEN;
+import { getAuthToken } from '@/lib/AxiosInstance';
 
 const ManageRoomPage = () => {
   const roomId = getCurrentRoomId();
+  const token = getAuthToken();
+
+  // roomId/token 없으면 소켓 연결 자체를 막아야 안전함
+  const safeRoomId = roomId ?? 0;       // 훅 내부에서 0이면 연결 안 하게 만들면 가장 깔끔
+  const safeToken = token ?? '';        // 빈 문자열이면 연결 안 하게
 
   const {
     participants,
     matchingAt,
     isMatched,
     isWagging,
-  } = useRoomSocket(roomId, TEST_TOKEN);
+  } = useRoomSocket(safeRoomId, safeToken);
 
   return (
     <S.Container>
       {!isMatched ? (
-        <ManageRoomDefault participants={participants} matching_at={matchingAt} Wagging={isWagging} />
+        <ManageRoomDefault
+          participants={participants}
+          matching_at={matchingAt}
+          Wagging={isWagging}
+        />
       ) : (
-        <ManageRoomMatched  />
+        <ManageRoomMatched />
       )}
     </S.Container>
   );
